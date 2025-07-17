2826c300ab892759e58eabb6c365aa3ee06e21da
svalvva <syalwalyrh@gmail.com>
2025-07-17 09:33:33 +0700

    user

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// User merepresentasikan data pengguna di collection 'users'
+type User struct {
+    ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+    Name     string             `json:"name" bson:"name"`
+    NIM      string             `json:"nim" bson:"nim"`
+    Email    string             `json:"email" bson:"email"`
+    Password string             `json:"-" bson:"password"` // Tanda - agar password tidak dikirim dalam JSON
+    Role     string             `json:"role" bson:"role"`
+}
