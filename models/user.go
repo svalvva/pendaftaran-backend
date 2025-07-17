40fc923aa326b8c2ef49470a44fd08653db44c5c
svalvva <syalwalyrh@gmail.com>
2025-07-17 13:08:38 +0700

    new

--- models/user.go
+++ models/user.go
@@ -2,12 +2,14 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// User merepresentasikan data pengguna di collection 'users'
 type User struct {
-    ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-    Name     string             `json:"name" bson:"name"`
-    NIM      string             `json:"nim" bson:"nim"`
-    Email    string             `json:"email" bson:"email"`
-    Password string 			`json:"password" bson:"password"` // Tanda - agar password tidak dikirim dalam JSON
-    Role     string             `json:"role" bson:"role"`
+    ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+    Name        string             `json:"name" bson:"name"`
+    NIM         string             `json:"nim" bson:"nim"`
+    Email       string             `json:"email" bson:"email"`
+    Password    string             `json:"password" bson:"password"`
+    Role        string             `json:"role" bson:"role"`
+    // TAMBAHKAN FIELD BARU DI SINI
+    BirthPlace  string             `json:"birth_place,omitempty" bson:"birth_place,omitempty"`
+    BirthDate   string             `json:"birth_date,omitempty" bson:"birth_date,omitempty"`
 }
