769930312768de6870e8987fc310a3c484321e03
svalvva <syalwalyrh@gmail.com>
2025-07-24 10:24:50 +0700

    user

--- models/user.go
+++ models/user.go
@@ -6,13 +6,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// User mendefinisikan struktur untuk data pengguna di dalam database.
-// Ini adalah satu-satunya model yang Anda butuhkan untuk pengguna.
+// User sekarang memiliki NIM dan Role
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	FullName  string             `bson:"full_name" json:"full_name" binding:"required"`
+	FullName  string             `bson:"full_name" json:"full_name"` // Hapus binding required agar tidak error saat login
 	Email     string             `bson:"email" json:"email" binding:"required,email"`
+	NIM       string             `bson:"nim" json:"nim" binding:"required"`
 	Password  string             `bson:"password" json:"password" binding:"required,min=6"`
+	Role      string             `bson:"role" json:"role"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
